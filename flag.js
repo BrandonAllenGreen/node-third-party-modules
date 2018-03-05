a208acf540716c76f7d162bd16f05233c4db6113
BrandonAllenGreen <green04@gmail.com>
2018-03-05 10:08:25 -0600

    flag is printing correctly

--- flag.js
+++ flag.js
@@ -1,6 +1,28 @@
+#!/usr/bin/env node
 'use strict';
 
 const chalk = require('chalk');
 
+// code is working but needs refactor
+const redStripe = chalk.bgRed('                                                  \n') //50 length
+const whiteStripe = chalk.bgWhite('                                                  \n') //50 length
+const sevenStars = chalk.white.bgBlue(' * * * * * * * ')
+const sixStars = chalk.white.bgBlue('  * * * * * *  ')
+const shortRedStripe = chalk.bgRed('                                   \n') //35 length
+const shortWhiteStripe = chalk.bgWhite('                                   \n') //35 length
 
-console.log(chalk.blue('Hello world!'));
+const USFlag = () => {
+  let a = sevenStars + shortRedStripe;
+  let b = sixStars + shortWhiteStripe;
+  let c = redStripe;
+  let d = whiteStripe;
+  let topPattern = a + b + a + b + a + b + a;
+  let bottomPattern = d + c + d + c + d + c;
+  console.log(`${topPattern}${bottomPattern}`);
+}
+  
+
+USFlag();
+
+
+
